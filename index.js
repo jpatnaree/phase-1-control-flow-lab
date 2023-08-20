4012deba9880dfa992ae38a068a1da2ed5bac7ae
Patnaree Johnson <j.patnaree.t@gmail.com>
2023-08-20 18:39:39 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number) {
+  let result
+  if(number <= 400) {
+  result = 'This one is on me!';
+  } else if(number <= 2000) {
+    result = 'That will be twenty bucks.';
+  } else if(number <= 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  } else {
+    result = 'No can do.';
+  }
+  return result 
+  }
+
+
+
+
+function ternaryCheckCity(city){
+  let result =
+  (city === "NYC" ? "Ok, sounds good." : "No go.")
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+      break;
+    case 'not as generous':
+      return "Thank you.";
+      break;
+    default:
+      return "Bye."
+  }
 }
